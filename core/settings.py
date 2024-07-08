6c4f30ce1f8fe86445254ba39e0fcd59ea32ebe9
Umaradigun <adigunfaruk09061@gmail.com>
2024-07-08 10:58:18 -0400

    edit conf

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG") != "False"
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 DEFAULT_APPS = [
